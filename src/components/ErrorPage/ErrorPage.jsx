0cf9e2b68519b2586e28173810cf42de58ac6018
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-09-21 14:06:54 +0100

    Improve ErrorPage styling & responsiveness

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router";
+import { Undo2 } from "lucide-react";
+import errorImage from "../../assets/images/404-cat.jpg";
+import styles from "./ErrorPage.module.css";
+
+function ErrorPage() {
+  return (
+    <div className={styles.ErrorPage}>
+      <div className={styles.messageContainer}>
+        <h1>We couldn't find the page you were looking for…but we found a sleeping cat instead!</h1>
+        <Link to="/" className={styles.homeButton}>
+          <Undo2 />
+          Go back
+        </Link>
+      </div>
+
+      <div className={styles.imageContainer}>
+        <img
+          src={errorImage}
+          alt="Error 404 image"
+          className={styles.errorImage}
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorPage;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,7 +2,7 @@ import App from "../App/App.jsx";
 import Shop from "../components/Shop/Shop.jsx";
 import Cart from "../components/Cart/Cart.jsx";
 import HomePage from "../components/HomePage/HomePage.jsx";
-import ErrorPage from "./ErrorPage.jsx";
+import ErrorPage from "../components/ErrorPage/ErrorPage.jsx";
 
 const routes = [
   {
