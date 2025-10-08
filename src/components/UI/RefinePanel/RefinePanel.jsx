89e4097414cbb541c351d9beeb63efd1576e9b03
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-10-08 12:45:06 +0100

    Fix both dropdowns opening at the same time

--- src/components/UI/RefinePanel/RefinePanel.jsx
+++ src/components/UI/RefinePanel/RefinePanel.jsx
@@ -1,55 +1,89 @@
 import { useState } from "react";
 import styles from "./RefinePanel.module.css";
-import { ChevronDown, LibraryBig } from "lucide-react";
+import { ChevronDown, LibraryBig, ArrowDownWideNarrow } from "lucide-react";
 
 function RefinePanel() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [openDropdown, setOpenDropdown] = useState(null);
 
-  const toggleMenu = () => setIsOpen(!isOpen);
+  const toggleMenu = (menuId) => {
+    setOpenDropdown((prev) => (prev === menuId ? null : menuId));
+  };
 
   return (
     <aside className={styles.refinePanel}>
       <div className={styles.categoryContainer}>
-        <div className={styles.categoryToggle} onClick={toggleMenu}>
+        <div
+          className={styles.categoryToggle}
+          onClick={() => toggleMenu("category")}
+        >
           <div>
             <LibraryBig />
             Category
           </div>
 
-          <ChevronDown />
+          <ChevronDown
+            className={`${styles.icon} ${
+              openDropdown === "category" ? styles.open : styles.closed
+            }`}
+          />
         </div>
 
-        <div
-          className={`${styles.categoryDropdown} ${
-            isOpen ? styles.open : styles.closed
-          }`}
-        >
-          <div className={styles.categoryContent}>
-            <label>
-              <input type="checkbox"></input>
-              Furniture
-            </label>
-            <label>
-              <input type="checkbox"></input>
-              Fragrance
-            </label>
-            <label>
-              <input type="checkbox"></input>
-              Home Decor
-            </label>
-            <label>
-              <input type="checkbox"></input>
-              Skincare
-            </label>
-            <label>
-              <input type="checkbox"></input>
-              Beauty
-            </label>
+        {openDropdown === "category" && (
+          <div
+            className={`${styles.categoryDropdown} ${
+              openDropdown ? styles.open : styles.closed
+            }`}
+          >
+            <div className={styles.categoryContent}>
+              <label>
+                <input type="checkbox"></input>
+                Furniture
+              </label>
+              <label>
+                <input type="checkbox"></input>
+                Fragrance
+              </label>
+              <label>
+                <input type="checkbox"></input>
+                Home Decor
+              </label>
+              <label>
+                <input type="checkbox"></input>
+                Skincare
+              </label>
+              <label>
+                <input type="checkbox"></input>
+                Beauty
+              </label>
+            </div>
           </div>
-        </div>
+        )}
       </div>
 
-      <div></div>
+      <div className={styles.sortContainer}>
+        <div className={styles.sortToggle} onClick={() => toggleMenu("sort")}>
+          <div>
+            <ArrowDownWideNarrow />
+            Sort
+          </div>
+
+          <ChevronDown
+            className={`${styles.icon} ${
+              openDropdown === "sort" ? styles.open : styles.closed
+            }`}
+          />
+        </div>
+
+        {openDropdown === "sort" && (
+          <div
+            className={`${styles.sortDropdown} ${
+              openDropdown ? styles.open : styles.closed
+            }`}
+          >
+            <div className={styles.sortContent}></div>
+          </div>
+        )}
+      </div>
     </aside>
   );
 }
