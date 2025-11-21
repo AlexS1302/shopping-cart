b7c0d5ded776754aa1ad5a2d1713b78b8d6a204e
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-11-21 14:49:20 +0000

    Improve header layout on mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,47 +1,85 @@
 import { Link } from "react-router";
-import { useEffect, useState } from "react";
 import { Menu } from "lucide-react";
 import SearchBar from "./SearchBar/SearchBar";
 import NavLinks from "./NavLinks/NavLinks";
 import logo from "../../assets/icons/logo.svg";
 import styles from "./Header.module.css";
-import useBreakpoint from "../../hooks/useBreakpoint/";
+import useBreakpoint from "../../hooks/useBreakpoint";
+import { useState, useEffect } from "react";
 
 function Header() {
   const isAbove1200 = useBreakpoint("(min-width: 1200px)");
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
-
   const toggleHamburger = () => setHamburgerOpen(!hamburgerOpen);
 
+  // prevent menu from remaining open if screen size is changed
   useEffect(() => {
     if (isAbove1200) {
       setHamburgerOpen(false);
     }
   }, [isAbove1200]);
 
+  useEffect(() => {
+    if (hamburgerOpen) {
+      document.body.classList.add(styles["noScroll"]);
+    } else {
+      document.body.classList.remove(styles["noScroll"]);
+    }
+  }, [hamburgerOpen]);
+
   return (
-    <div className={styles.Header}>
-      <nav>
-        <h1 className={styles.headerTitle}>
-          <Link to="/">
-            <img src={logo} alt="Plentiful Logo" className={styles.logo}></img>
-            Plentiful
-          </Link>
-        </h1>
+    <div className={`${styles.Header} ${hamburgerOpen ? styles.expand : ""}`}>
+      <nav className={`${hamburgerOpen ? styles.hamburgerNav : ""}`}>
+        {isAbove1200 ? (
+          <>
+            <h1 className={styles.headerTitle}>
+              <Link to="/" onClick={() => setHamburgerOpen(false)}>
+                <img
+                  src={logo}
+                  alt="Plentiful Logo"
+                  className={styles.logo}
+                ></img>
+                Plentiful
+              </Link>
+            </h1>
 
-        <SearchBar />
+            <SearchBar hamburgerOpen={hamburgerOpen} />
 
-        <div className={styles.desktopLinks}>
-          <NavLinks />
-        </div>
+            <div className={styles.desktopLinks}>
+              <NavLinks />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.mobileLeftSide}>
+              <h1 className={styles.headerTitle}>
+                <Link to="/" onClick={() => setHamburgerOpen(false)}>
+                  <img
+                    src={logo}
+                    alt="Plentiful Logo"
+                    className={styles.logo}
+                  ></img>
+                  Plentiful
+                </Link>
+              </h1>
+            </div>
 
-        {/* Mobile */}
-        <Menu className={styles.hamburgerIcon} onClick={toggleHamburger} />
+            <div className={styles.mobileRightSide}>
+              {hamburgerOpen ? null : (
+                <SearchBar hamburgerOpen={hamburgerOpen} />
+              )}
 
-        {hamburgerOpen && (
-          <div className={styles.mobileLinks}>
-            <NavLinks onClick={() => setHamburgerOpen(false)} />
-          </div>
+              <Menu
+                className={styles.hamburgerIcon}
+                onClick={toggleHamburger}
+              />
+            </div>
+            {hamburgerOpen && (
+              <div className={styles.mobileLinks}>
+                <NavLinks onClick={() => setHamburgerOpen(false)} />
+              </div>
+            )}
+          </>
         )}
       </nav>
     </div>
--- src/components/Header/SearchBar/SearchBar.jsx
+++ src/components/Header/SearchBar/SearchBar.jsx
@@ -2,8 +2,10 @@ import styles from "./SearchBar.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { Search } from "lucide-react";
+import useBreakpoint from "../../../hooks/useBreakpoint";
 
-function SearchBar() {
+function SearchBar({ hamburgerOpen }) {
+  const isAbove1200 = useBreakpoint("(min-width: 1200px)");
   const navigate = useNavigate();
   const [searchInput, setSearchInput] = useState("");
 
@@ -24,26 +26,39 @@ function SearchBar() {
   };
 
   return (
-    <div className={styles.searchSection}>
-      <Search className={styles.searchIcon} />
-      <input
-        id="search"
-        name="search"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-        onKeyDown={handleKeyDown}
-        placeholder="Fragrance"
-        size="30"
-        className={styles.searchInput}
-      ></input>
-      <button
-        type="button"
-        className={styles.searchButton}
-        onClick={handleSearch}
-      >
-        Search
-      </button>
-    </div>
+    <>
+      {isAbove1200 ? (
+        <div
+          className={`${styles.searchSection} ${
+            hamburgerOpen ? styles.hidden : ""
+          }`}
+        >
+          <Search className={styles.searchIcon} />
+          <input
+            id="search"
+            name="search"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            onKeyDown={handleKeyDown}
+            placeholder="Fragrance"
+            size="30"
+            className={styles.searchInput}
+          ></input>
+          <button
+            type="button"
+            className={styles.searchButton}
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+      ) : (
+        <div className={styles.searchSection}>
+          <Search className={styles.searchIcon} />
+        </div>
+      )}
+    </>
   );
 }
+
 export default SearchBar;
