cb3b88d141b86279c435546d72612f4a56210683
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-12-19 18:04:21 +0000

    Extract product skeleton into its own component

--- src/components/UI/ProductCards/ProductCards.jsx
+++ src/components/UI/ProductCards/ProductCards.jsx
@@ -1,8 +1,7 @@
 import styles from "./ProductCards.module.css";
 import { Star, Heart } from "lucide-react";
 import { useOutletContext } from "react-router";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import ProductSkeleton from "./ProductSkeleton/ProductSkeleton";
 import AddToCartButton from "../../Pages/Shop/AddToCartButton/AddToCartButton";
 
 function ProductCards({ product, loading }) {
@@ -21,29 +20,7 @@ function ProductCards({ product, loading }) {
   return (
     <article className={styles.productCard}>
       {loading ? (
-        <>
-          <Skeleton className={styles.productThumbnail} />
-
-          <div className={styles.productText}>
-            <div className={styles.productCategory}>
-              <Skeleton width={100} height={20} />
-            </div>
-            <div className={styles.productTitle}>
-              <Skeleton height={20} />
-            </div>
-            <div className={styles.productDesc}>
-              <Skeleton height={20} count={3} />
-            </div>
-          </div>
-
-          <div className={styles.productMetrics}>
-            <Skeleton height={30} containerClassName={styles.flex1} />
-          </div>
-
-          <div className={styles.skeletonButton}>
-            <Skeleton height={50} />
-          </div>
-        </>
+        <ProductSkeleton />
       ) : (
         <>
           <Heart
---
+++ src/components/UI/ProductCards/ProductSkeleton/ProductSkeleton.jsx
@@ -0,0 +1,32 @@
+import styles from ".././ProductCards.module.css";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+function ProductSkeleton() {
+  return (
+    <>
+      <Skeleton className={styles.productThumbnail} />
+
+      <div className={styles.productText}>
+        <div className={styles.productCategory}>
+          <Skeleton width={100} height={20} />
+        </div>
+        <div className={styles.productTitle}>
+          <Skeleton height={20} />
+        </div>
+        <div className={styles.productDesc}>
+          <Skeleton height={20} count={3} />
+        </div>
+      </div>
+
+      <div className={styles.productMetrics}>
+        <Skeleton height={30} containerClassName={styles.flex1} />
+      </div>
+
+      <div className={styles.skeletonButton}>
+        <Skeleton height={50} />
+      </div>
+    </>
+  );
+}
+export default ProductSkeleton;
